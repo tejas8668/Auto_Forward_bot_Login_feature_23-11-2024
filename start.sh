322c17a2a98a3450b8227e430259aa75a1bcfa30
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2024-11-23 20:47:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/Auto_Forward_bot_Login_feature_23-11-2024 /LazyDeve
 cd /LazyDeveloper
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+python main.py
