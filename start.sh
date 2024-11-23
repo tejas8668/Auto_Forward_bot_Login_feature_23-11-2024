ffe1e9d5241fc6792e89953858cff2a907d3431b
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2024-11-23 20:47:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-
+echo "Cloning Repo...."
+git clone https://github.com/Auto_Forward_bot_Login_feature_23-11-2024 /LazyDeveloper
+cd /LazyDeveloper
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
